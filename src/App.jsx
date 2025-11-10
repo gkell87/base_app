3aacc68a5afae47e60b7f0046a71e6b6c60ee1b4
gkell87 <g_kell87@yahoo.com>
2025-11-10 18:51:51 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="landing-page-container">
+      {/* <h1 className="about-us-heading">About Us</h1> */}
+      <p className="landing-page-description">Welcome to my App!</p>
+      <p className="landing-page-content">
+        place holder.
+      </p>
+      <p className="landing-page-content">
+        place holder.
+      </p>
+      <p className="landing-page-content">
+        place holder.
+      </p>
+    </div>
+  );
+}
+
+export default App;
